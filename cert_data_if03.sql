7fdaea4bf20c1161619ae6a5f5c037ff0c03e02d
Juan David Linares <juan.linares@geostudio.com.co>
2022-11-08 15:28:02 +0000

    certificacion cifras saldos IF03

--- cert_data_if03.sql
+++ cert_data_if03.sql
@@ -31,3 +31,37 @@ SELECT 2022 as anio,
     SUM(total_coloc)::int as total_coloc,
     SUM(valor_coloc)::real as valor_coloc
 FROM colocaciones_geo_reg_if03;
+
+-- Saldos IF03 cifras totales
+SELECT 'saldos_if03' as tabla,
+    mes::int,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_saldo)::real as valor_coloc
+FROM saldos_if03
+WHERE anio = 2022
+GROUP BY 2
+UNION
+-- esta union corresponde a los totales del anio corrido (vigencia corrida representada por el valor de 0 en el campo mes)
+SELECT 'saldos_if03' as tabla,
+    0 as mes,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_saldo)::real as valor_coloc
+FROM saldos_if03
+WHERE anio = 2022
+GROUP BY 2
+ORDER BY 1, 2;
+-- Colocaciones IF03 Cifras capa geografica (municipios)
+SELECT 2022 as anio,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_saldo)::real as valor_coloc
+FROM saldos_geo_if03;
+-- Colocaciones IF03 Cifras capa geografica (departamentos)
+SELECT 2022 as anio,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_saldo)::real as valor_coloc
+FROM saldos_geodep_if03;
+-- Colocaciones IF03 Cifras capa geografica (regiones)
+SELECT 2022 as anio,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_saldo)::real as valor_coloc
+FROM saldos_georeg_if03;
