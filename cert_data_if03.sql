773cd31879263624a1af91718c7a69bb6fa62c55
Juan David Linares <juan.linares@geostudio.com.co>
2022-11-08 19:28:54 +0000

    certificacion cifras ISA IF03

--- cert_data_if03.sql
+++ cert_data_if03.sql
@@ -228,3 +228,44 @@ SELECT 2022 as anio,
     SUM(valor_inicial)::real as valor_inicial,
     SUM(valor_actual)::real as valor_actual
 FROM fagvig_georeg_if03;
+
+-- ISA IF03 cifras totales
+SELECT 'isaunaseg_if03' as tabla,
+    mes_vigencia::int as mes,
+    SUM(total_unaseg)::int as total_unaseg,
+    SUM(valor_aseg)::real as valor_aseg,
+    SUM(valor_incisa)::real as valor_incisa,
+    SUM(valor_prima)::real as valor_prima
+FROM isaunaseg_if03
+WHERE anio_vigencia = 2022
+GROUP BY 2
+UNION
+-- esta union corresponde a los totales del anio corrido (vigencia corrida representada por el valor de 0 en el campo mes)
+SELECT 'isaunaseg_if03' as tabla,
+    0 as mes,
+    SUM(total_unaseg)::int as total_unaseg,
+    SUM(valor_aseg)::real as valor_aseg,
+    SUM(valor_incisa)::real as valor_incisa,
+    SUM(valor_prima)::real as valor_prima
+FROM isaunaseg_if03
+WHERE anio_vigencia = 2022
+GROUP BY 2
+ORDER BY 1, 2;
+-- ISA IF03 Cifras capa geografica (municipios)
+SELECT 2022 as anio,
+    SUM(total_unaseg)::int as total_unaseg,
+    SUM(valor_asegurado)::real as valor_aseg,
+    SUM(valor_incentivo)::real as valor_incisa
+FROM isapol_geo_if03;
+-- ISA IF03 Cifras capa geografica (departamentos)
+SELECT 2022 as anio,
+    SUM(total_unaseg)::int as total_unaseg,
+    SUM(valor_asegurado)::real as valor_aseg,
+    SUM(valor_incentivo)::real as valor_incisa
+FROM isapol_geodep_if03;
+-- ISA IF03 Cifras capa geografica (regiones)
+SELECT 2022 as anio,
+    SUM(total_unaseg)::int as total_unaseg,
+    SUM(valor_asegurado)::real as valor_aseg,
+    SUM(valor_incentivo)::real as valor_incisa
+FROM isapol_georeg_if03;
