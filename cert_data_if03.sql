b47b748a7acd9125089e36ec59a4c5e5bef857d6
Juan David Linares <juan.linares@geostudio.com.co>
2022-11-08 16:33:15 +0000

    certificacion cifras FAG pagadas IF03

--- cert_data_if03.sql
+++ cert_data_if03.sql
@@ -113,7 +113,7 @@ WHERE anio = 2022
 GROUP BY 2
 UNION
 -- esta union corresponde a los totales del anio corrido (vigencia corrida representada por el valor de 0 en el campo mes)
-SELECT 'lec_if03' as tabla,
+SELECT 'fagexp_if03' as tabla,
     0 as mes,
     SUM(total_cert)::int as total_cert,
     SUM(valor_garant)::real as valor_garant,
@@ -140,3 +140,47 @@ SELECT 2022 as anio,
     SUM(valor_garant)::real as valor_garant,
     SUM(valor_credfag)::real as valor_credfag
 FROM fagexp_georeg_if03;
+
+-- FAG Pagadas IF03 cifras totales
+SELECT 'fagpag_if03' as tabla,
+    mes::int,
+    SUM(total_cert)::int as total_cert,
+    SUM(valor_credgar)::real as valor_credgar,
+    SUM(valor_inicial)::real as valor_inicial,
+    SUM(valor_pagado)::real as valor_pagado
+FROM fagpag_if03
+WHERE anio = 2022
+GROUP BY 2
+UNION
+-- esta union corresponde a los totales del anio corrido (vigencia corrida representada por el valor de 0 en el campo mes)
+SELECT 'fagpag_if03' as tabla,
+    0 as mes,
+    SUM(total_cert)::int as total_cert,
+    SUM(valor_credgar)::real as valor_credgar,
+    SUM(valor_inicial)::real as valor_inicial,
+    SUM(valor_pagado)::real as valor_pagado
+FROM fagpag_if03
+WHERE anio = 2022
+GROUP BY 2
+ORDER BY 1, 2;
+-- FAG Pagadas IF03 Cifras capa geografica (municipios)
+SELECT 2022 as anio,
+    SUM(total_cert)::int as total_cert,
+    SUM(valor_credgar)::real as valor_credgar,
+    SUM(valor_inicial)::real as valor_inicial,
+    SUM(valor_pagado)::real as valor_pagado
+FROM fagpag_geo_if03;
+-- FAG Pagadas IF03 Cifras capa geografica (departamentos)
+SELECT 2022 as anio,
+    SUM(total_cert)::int as total_cert,
+    SUM(valor_credgar)::real as valor_credgar,
+    SUM(valor_inicial)::real as valor_inicial,
+    SUM(valor_pagado)::real as valor_pagado
+FROM fagpag_geodep_if03;
+-- FAG Pagadas IF03 Cifras capa geografica (regiones)
+SELECT 2022 as anio,
+    SUM(total_cert)::int as total_cert,
+    SUM(valor_credgar)::real as valor_credgar,
+    SUM(valor_inicial)::real as valor_inicial,
+    SUM(valor_pagado)::real as valor_pagado
+FROM fagpag_georeg_if03;
