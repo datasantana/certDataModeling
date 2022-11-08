58b9249e21076f6636304777020d0b14733e8f82
Juan David Linares <juan.linares@geostudio.com.co>
2022-11-08 16:16:20 +0000

    certificacion cifras LEC IF03

--- cert_data_if03.sql
+++ cert_data_if03.sql
@@ -65,3 +65,37 @@ SELECT 2022 as anio,
     SUM(total_coloc)::int as total_coloc,
     SUM(valor_saldo)::real as valor_coloc
 FROM saldos_georeg_if03;
+
+-- LEC IF03 cifras totales
+SELECT 'lec_if03' as tabla,
+    mes::int,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc)::real as valor_coloc
+FROM lec_if03
+WHERE anio = 2022
+GROUP BY 2
+UNION
+-- esta union corresponde a los totales del anio corrido (vigencia corrida representada por el valor de 0 en el campo mes)
+SELECT 'lec_if03' as tabla,
+    0 as mes,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc)::real as valor_coloc
+FROM lec_if03
+WHERE anio = 2022
+GROUP BY 2
+ORDER BY 1, 2;
+-- LEC IF03 Cifras capa geografica (municipios)
+SELECT 2022 as anio,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc)::real as valor_coloc
+FROM lec_geo_if03;
+-- LEC IF03 Cifras capa geografica (departamentos)
+SELECT 2022 as anio,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc)::real as valor_coloc
+FROM lec_geodep_if03;
+-- Colocaciones IF03 Cifras capa geografica (regiones)
+SELECT 2022 as anio,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc)::real as valor_coloc
+FROM lec_georeg_if03;
