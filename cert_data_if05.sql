1f3ccb116d0a1c6408a0cce9091334126e4fa138
Juan David Linares <juan.linares@geostudio.com.co>
2022-11-09 15:17:50 +0000

    certificacion cifras Credito IF05B

--- cert_data_if05.sql
+++ cert_data_if05.sql
@@ -54,3 +54,80 @@ SELECT anio_vigencia::int,
 FROM isahist_geo_if05a
 GROUP BY 1
 ORDER BY 1;
+
+-- Credito colocaciones IF05B cifras totales (valores corrientes)
+SELECT anio::int,
+    'Total' as tipo_linea,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc)::real as valor_coloc
+FROM colocacionhist_if05b
+GROUP BY 1
+UNION
+SELECT anio::int,
+    tipo_linea,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc)::real as valor_coloc
+FROM colocacionhist_if05b
+GROUP BY 1, 2
+ORDER BY 1, 2;
+
+-- Credito saldos IF05B cifras totales (valores corrientes)
+SELECT anio::int,
+    'Total' as tipo_linea,
+    SUM(total_saldos)::int as total_saldos,
+    SUM(valor_saldos)::real as valor_saldos
+FROM saldoshist_if05_b
+GROUP BY 1
+UNION
+SELECT anio::int,
+    tipo_linea,
+    SUM(total_saldos)::int as total_saldos,
+    SUM(valor_saldos)::real as valor_saldos
+FROM saldoshist_if05_b
+GROUP BY 1, 2
+ORDER BY 1, 2;
+
+-- Credito colocaciones IF05B cifras resumidas (valores constantes y corrientes)
+SELECT anio::int,
+    'Total' as tipo_linea,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc_cons)::real as valor_coloc_cons,
+    SUM(valor_coloc)::real as valor_coloc
+FROM colochist_tipoylinea
+GROUP BY 1
+UNION
+SELECT anio::int,
+    tipo_linea,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc_cons)::real as valor_coloc_cons,
+    SUM(valor_coloc)::real as valor_coloc
+FROM colochist_tipoylinea
+GROUP BY 1, 2
+ORDER BY 1, 2;
+
+-- Credito saldos IF05B cifras resumidas (valores constantes y corrientes)
+SELECT anio::int,
+    'Total' as tipo_linea,
+    SUM(total_saldos)::int as total_saldos,
+    SUM(valor_saldo_cons)::real as valor_saldos_cons,
+    SUM(valor_saldos)::real as valor_saldos
+FROM saldoshist_tipoylinea
+GROUP BY 1
+UNION
+SELECT anio::int,
+    tipo_linea,
+    SUM(total_saldos)::int as total_saldos,
+    SUM(valor_saldo_cons)::real as valor_saldos_cons,
+    SUM(valor_saldos)::real as valor_saldos
+FROM saldoshist_tipoylinea
+GROUP BY 1, 2
+ORDER BY 1, 2;
+
+-- Colocaciones IF05B cifras capa geografica (municipios)
+SELECT anio::int,
+    SUM(total_coloc)::int as total_coloc,
+    SUM(valor_coloc_cons)::real as valor_coloc_cons,
+    SUM(valor_coloc)::real as valor_coloc
+FROM colochist_geo_if05_b
+GROUP BY 1
+ORDER BY 1;
