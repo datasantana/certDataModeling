0820ad2b5651efde6c1901a8ed05c2a57f239b01
Juan David Linares <juan.linares@geostudio.com.co>
2022-11-09 16:00:13 +0000

    cerficacion cifras FMR IF06

--- cert_data_if06.sql
+++ cert_data_if06.sql
@@ -102,3 +102,23 @@ FROM isamjr_if06
 WHERE anio_vigencia = 2022
 GROUP BY 1, 2
 ORDER BY 1, 2;
+
+-- FMR IF06 cifras totales
+SELECT mes::int,
+    'Total Persona Natural' as sexo,
+    SUM(total_operac)::int as total_operac,
+    SUM(valor_cred)::real as valor_cred
+FROM mfrmjr_if06
+WHERE anio = 2022
+GROUP BY 1, 2
+UNION
+-- esta union corresponde a los totales del anio corrido
+SELECT mes::int,
+    sexo,
+    SUM(total_operac)::int as total_operac,
+    SUM(valor_cred)::real as valor_cred
+FROM mfrmjr_if06
+WHERE anio = 2022
+GROUP BY 1, 2
+ORDER BY 1, 2;
+
