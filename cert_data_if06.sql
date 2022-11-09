4d57125850b71cdd39855e94860fed7551e774c6
Juan David Linares <juan.linares@geostudio.com.co>
2022-11-09 15:53:06 +0000

    certificacion cifras ICR IF06

--- cert_data_if06.sql
+++ cert_data_if06.sql
@@ -56,3 +56,26 @@ FROM fagmjr_if06
 WHERE anio = 2022
 GROUP BY 1, 2
 ORDER BY 1, 2;
+
+-- ICR IF06 cifras totales
+SELECT mes::int,
+    'Total Persona Natural' as sexo,
+    SUM(total_proy)::int as total_proy,
+    SUM(valor_pagado)::real as valor_pagado,
+    SUM(valor_credicr)::real as valor_credicr,
+    SUM(valor_inver)::real as valor_inver
+FROM icrmjr_if06
+WHERE anio = 2022
+GROUP BY 1, 2
+UNION
+-- esta union corresponde a los totales del anio corrido
+SELECT mes::int,
+    sexo,
+    SUM(total_proy)::int as total_proy,
+    SUM(valor_pagado)::real as valor_pagado,
+    SUM(valor_credicr)::real as valor_credicr,
+    SUM(valor_inver)::real as valor_inver
+FROM icrmjr_if06
+WHERE anio = 2022
+GROUP BY 1, 2
+ORDER BY 1, 2;
