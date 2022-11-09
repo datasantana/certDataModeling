003f0b7508816737083bcba214233e3c32b45dda
Juan David Linares <juan.linares@geostudio.com.co>
2022-11-09 14:01:09 +0000

    certificacion cifras ICR IF02

--- cert_data_if02.sql
+++ cert_data_if02.sql
@@ -28,3 +28,33 @@ SELECT 2022 as anio,
     SUM(valor_valaseg)::real as valor_aseg,
     SUM(valor_incisa)::real as valor_incisa
 FROM isapol_geo_if02;
+
+-- ICR IF02 cifras totales
+SELECT 'icrproy_if02' as tabla,
+    mes::int,
+    SUM(total_proy)::int as total_proy,
+    SUM(valor_pagado)::real as valor_pagicr,
+    SUM(valor_credicr)::real as valor_credicr,
+    SUM(valor_inver)::real as valor_inver
+FROM icrproy_if02
+WHERE anio = 2022
+GROUP BY 2
+UNION
+-- esta union corresponde a los totales del anio corrido (vigencia corrida representada por el valor de 0 en el campo mes)
+SELECT 'icrproy_if02' as tabla,
+    0 as mes,
+    SUM(total_proy)::int as total_proy,
+    SUM(valor_pagado)::real as valor_pagicr,
+    SUM(valor_credicr)::real as valor_credicr,
+    SUM(valor_inver)::real as valor_inver
+FROM icrproy_if02
+WHERE anio = 2022
+GROUP BY 2
+ORDER BY 1, 2;
+-- ICR IF02 Cifras capa geografica (municipios)
+SELECT 2022 as anio,
+    SUM(total_proy)::int as total_proy,
+    SUM(valor_pagado)::real as valor_pagicr,
+    SUM(valor_credicr)::real as valor_credicr,
+    SUM(valor_inver)::real as valor_inver
+FROM icrproy_geo_if02;
