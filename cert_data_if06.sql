664e61416eed0567a09174faea8b943822351673
Juan David Linares <juan.linares@geostudio.com.co>
2022-11-09 15:44:30 +0000

    certificacion cifras LEC IF06

--- cert_data_if06.sql
+++ cert_data_if06.sql
@@ -16,3 +16,22 @@ FROM colocmjr_if06
 WHERE anio = 2022
 GROUP BY 1, 2
 ORDER BY 1, 2;
+
+-- LEC IF06 cifras totales
+SELECT mes::int,
+    'Total Persona Natural' as sexo,
+    SUM(total_colec)::int as total_colec,
+    SUM(valor_colec)::real as valor_colec
+FROM lecmjr_if06
+WHERE anio = 2022
+GROUP BY 1, 2
+UNION
+-- esta union corresponde a los totales del anio corrido
+SELECT mes::int,
+    sexo,
+    SUM(total_colec)::int as total_colec,
+    SUM(valor_colec)::real as valor_colec
+FROM lecmjr_if06
+WHERE anio = 2022
+GROUP BY 1, 2
+ORDER BY 1, 2;
